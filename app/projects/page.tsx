eb4d57e3f44d5fa6b0638922f9bbee1350cc8825
Julia Zet <juliazet@gmx.de>
2023-11-07 15:26:47 +0100

    personalise projects page, text and images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,8 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Some projects and concepts that I`ve got to know during my travels – or that I simply
+            want to support.
           </p>
         </div>
         <div className="container py-12">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Permaculture Principles',
+    description: `Permaculture is a holistic design approach based on patterns and relations that we can observe in nature. It unifies various principles to guide people how to create, produce, live and thrive in a healthy community. It's mainly focusing on organised crop rotation that keeps the micro-organisms in the soil alive. It follows principles like earth care, fair share and people care.`,
+    imgSrc: '/static/images/beth-macdonald-permaculture.jpg',
+    href: 'https://permacultureprinciples.com/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Blue Zones',
+    description: `Live healthy and, as a result, live longer! The Blue Zones describe various places on earth where people live to an extraordinary old age. Dan Buettner explored regions in Japan, Italy, Greece, Costa Rica and even California to find out why people are living to 100 years and older. His organization explains the science behind it and transforms communities to promote longevity.`,
+    imgSrc: '/static/images/dall-e-blue-zones.png',
+    href: 'https://www.bluezones.com/',
   },
 ]
 
